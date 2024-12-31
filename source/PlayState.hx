fc35b2bd50fb95a3f10b2e19a462e43d8fa91c0c
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-12-31 17:46:20 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,41 +1,61 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
+import flixel.group.FlxTypedSpriteGroup;
 import haxe.Json;
-import haxe.ds.Vector;
 import sys.io.File;
 
 class PlayState extends FlxState
 {
+	private var mainHand:Array<FlxCharacter>;
+	private var avaliable:Array<FlxCharacter>;
+		
 	private var characters:Array<FlxCharacter> = [
-		new FlxCharacter("Arthur.json", 1),
-		new FlxCharacter("Ass.json", 1),
-		new FlxCharacter("Ednaldo.json", 1),
-		new FlxCharacter("Eduardo.json", 1),
-		new FlxCharacter("Elon.json", 1),
-		new FlxCharacter("Granny.json", 1),
-		new FlxCharacter("Hitler.json", 1),
-		new FlxCharacter("Marco.json", 1),
-		new FlxCharacter("Thais.json", 1),
-		new FlxCharacter("Witch71.json", 1),
-		new FlxCharacter("Psychoduck.json", 1),
-		new FlxCharacter("CreoleBart.json", 1),
-		new FlxCharacter("MatheusGirl.json", 1),
-		new FlxCharacter("Protozoario.json", 1)
+		new FlxCharacter("Arthur",      1, 0 ),
+		new FlxCharacter("Ass",         1, 1 ),
+		new FlxCharacter("Ednaldo",     1, 2 ),
+		new FlxCharacter("Eduardo",     1, 3 ),
+		new FlxCharacter("Elon",        1, 4 ),
+		new FlxCharacter("Granny",      1, 5 ),
+		new FlxCharacter("Hitler",      1, 6 ),
+		new FlxCharacter("Marco",       1, 7 ),
+		new FlxCharacter("Thais",       1, 8 ),
+		new FlxCharacter("Witch71",     1, 9 ),
+		new FlxCharacter("Psychoduck",  1, 10),
+		new FlxCharacter("CreoleBart",  1, 11),
+		new FlxCharacter("MatheusGirl", 1, 12),
+		new FlxCharacter("Protozoario", 1, 13),
+		new FlxCharacter("IdiotThief",  1, 14)
 	];
 	
-	private var deck:Vector<FlxCharacter> = new Vector(8);
+	private var deck:FlxTypedSpriteGroup<FlxCharacter>;
 	
 	override public function create()
 	{
 		super.create();
 		
-		// Testes
+		deck = new FlxTypedSpriteGroup<FlxCharacter>();
+		deck.maxSize = 8;
 		
-		for(i in 0...deck.length)
+		avaliable = deck.members.copy();
+		
+		hand = [];
+		
+		for(i in 0 ... Std.int(Math.min(8, characters.length)))
 		{
-			deck[i] = characters[i];
+			deck.add(character[i]);
+		}
+		
+		for(i in 0 ... Std.int(Math.min(4, deck.length)))
+		{
+			mainHand.add(deck[i]);
+		}
+		
+		while(hand.length < 4 && avaliable.length - 1)
+		{
+			
 		}
 	}
 
@@ -49,6 +69,8 @@ class FlxCharacter extends FlxSprite
 {
 	private var json:Dynamic = {};
 	
+	private var identifier:Int;
+	
 	private var hitpoints:Int;
 	private var damage:Int;
 	
@@ -61,13 +83,27 @@ class FlxCharacter extends FlxSprite
 	
 	private var hasEvolution:Bool;
 	
-	public function new(name:String, level:Int)
+	private var evoltion:Dynamic = {};
+	
+	public function new(name:String, level:Int, id:Int)
 	{
 		super();
 		
-		json = Json.parse(File.getContent("assets/data/characters/" + name));
+		if(level < 1 || level > 9)
+		{
+			level = 1;
+		}
 		
-		loadGraphic("assets/images/characters/" + name);
+		if(id > 0)
+		{
+			identifier = id;
+		}
+		
+		json = Json.parse(File.getContent("assets/data/characters/" + name + ".json"));
+		
+		evolution = json.evolutionExtras[level - 1];
+		
+		loadGraphic("assets/images/characters/" + name + ".jpg");
 		
 		currentLvl = level;
 		
@@ -79,28 +115,48 @@ class FlxCharacter extends FlxSprite
 		
 		hasEvolution = json.hasEvolution;
 		
-		Sys.println(attributeToString("Name: " + json.name));
-		Sys.println(attributeToString("Description: " + json.description));
-		Sys.println("Levels and upgrades:");
+		Sys.println(attributeToString("Nome: " + json.name));
+		Sys.println(attributeToString("Descrição: " + json.description));
+		Sys.println("Níveis e melhorias:");
 		
 		// Informação importante: Os níveis vão de 1 à 9 por padrão
 		
 		for(i in 1...10)
 		{
-			Sys.println(i + " HP: " + attributeToString(json.statusPerLvl[i - 1].hp));
-			Sys.println(i + " DMG: " + attributeToString(json.statusPerLvl[i - 1].dmg));
+			Sys.println(i + " HP: " + attributeToString(evolution.hp));
+			Sys.println(i + " DMG: " + attributeToString(evolution[i - 1].dmg));
 		}
 		
-		Sys.println("Has evolution: " + attributeToString(hasEvolution));
+		Sys.println("Tem evolução: " + attributeToString(hasEvolution));
 		
 		if(hasEvolution)
 		{
-			Sys.println("Evolution: " + attributeToString(json.evolutionExtras));
+			Sys.println("Evolução: " + attributeToString(json.evolutionExtras));
 		}
 		
-		Sys.println("Attack delay: " + attributeToString(json.status.atkSpeed));
-		Sys.println("Meters/s: " + attributeToString(json.status.movSpeed));
-		Sys.println("Atk. range: " + attributeToString(json.status.atkRange));
+		Sys.println("Intervalo de ataque: " + attributeToString(json.status.atkSpeed));
+		Sys.println("Metros por segundo: " + attributeToString(json.status.movSpeed));
+		Sys.println("Alcance de ataque: " + attributeToString(json.status.atkRange));
+		
+		scale.set(0.165, 0.165);
+		
+		centerOrigin();
+	}
+	
+	public function upgrade():Void
+	{
+		currentLvl++;
+		
+		hitpoints = json.statusPerLvl[currentLvl - 1].hp;
+		damage = json.statusPerLvl[currentLvl - 1].dmg;
+	}
+	
+	public function evolve():Void
+	{
+		if(hasEvolution)
+		{
+			evolution = json.evolutionExtras[currentLvl - 1]
+		}
 	}
 	
 	public function attributeToString(selected:Dynamic):String
